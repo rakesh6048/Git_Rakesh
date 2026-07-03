bc21d7e43caa36db89c40d0de133b488118ad72e
rakesh <rakesh.s6048@gmail.com>
2026-07-03 16:51:13 +0530

    Changes Done By Ammerican Guy

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -16,7 +16,9 @@ public class SampleTest {
          System.out.println(data.get(5));   
          System.out.println(data.get(6));   
          System.out.println(data.get(7)); 
-         System.out.println(data.get(8));   
+         System.out.println(data.get(8)); 
+         System.out.println(data.get(9)); 
+         System.out.println(data.get(10)); 
 		
 		
 	}
